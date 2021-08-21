f2efaa4c0a3854123d980a3b9d893c5044b5244d
Chasefy <88058422+Chasefy@users.noreply.github.com>
2021-08-21 11:38:04 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+
+function preload(){
+
+}
+
+function setup(){
+  createCanvas(1000,1800);
+  
+}
+
+function draw() {
+  background("purple");
+textAlign(CENTER, TOP);
+textFont("Impact");
+fill("black");
+strokeWeight(5);
+text("SipWithNika",500, 100);
+ drawSprites();
+}
